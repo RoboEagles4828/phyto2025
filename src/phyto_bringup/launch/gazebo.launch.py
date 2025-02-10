357b966f304df591edb9397c47a32331b675f271
ugackMiner53 <ugackMiner@gmail.com>
2025-02-10 23:06:03 +0000

    Gazebo changes

--- src/phyto_bringup/launch/gazebo.launch.py
+++ src/phyto_bringup/launch/gazebo.launch.py
@@ -1,31 +1,53 @@
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription, SetLaunchConfiguration
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, TextSubstitution
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
 
+import os
 
 def generate_launch_description():
-    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    pkg_phyto_description = get_package_share_directory('phyto_description')
+    project_root = '/workspaces/phyto2025'
+    pkg_ros_gz_sim = FindPackageShare('ros_gz_sim')
     gz_launch_path = PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
-    gz_model_path = PathJoinSubstitution([pkg_phyto_description, 'meshes'])
+
+    declare_world = DeclareLaunchArgument(
+        'world',
+        default_value='empty',
+        choices=['frc2025', 'empty'],
+        description='World to load into Gazebo'
+    )
+
+
+    
+    gazebo_layer = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(gz_launch_path),
+        launch_arguments={
+            'gz_args': ["empty.sdf"],
+            'on_exit_shutdown': 'True'
+        }.items(),
+    )
+
+
+
+
+    load_nodes = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        parameters=[{
+            'world': 'empty',
+            'file': PathJoinSubstitution([project_root, 'src/phyto_description/urdf/robot.urdf'])
+            # 'file': 'empty.sdf'
+        }]
+    )
+
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'world',
-            default_value='empty',
-            choices=['empty', 'mars', 'enceladus'],
-            description='World to load into Gazebo'
-        ),
+        declare_world,
         SetLaunchConfiguration(name='world_file', value=[LaunchConfiguration('world'), TextSubstitution(text='.sdf')]),
-        SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', gz_model_path),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gz_launch_path),
-            launch_arguments={
-                'gz_args': ["empty.sdf"],
-                'on_exit_shutdown': 'True'
-            }.items(),
-        )
+        SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', PathJoinSubstitution([project_root, 'src'])),
+        gazebo_layer,
+        load_nodes
     ])
