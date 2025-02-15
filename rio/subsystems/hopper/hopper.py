da6628cb0097a4adac978f93616c47af99352c04
RedHedgehog7448 <harper.greenwood@gmail.com>
2025-02-15 16:24:55 +0000

    Code for hopper motor

---
+++ rio/subsystems/hopper/hopper.py
@@ -0,0 +1,15 @@
+from phoenix5 import TalonSRX, TalonSRXControlMode
+from commands2 import Subsystem
+
+class Hopper(Subsystem):
+    def _init_(self):
+        self.motor = TalonSRX
+
+    def sethopperspeed(self, percentOutput):
+        self.motor.set(TalonSRXControlMode.PercentOutput, percentOutput)
+
+    def stop(self):
+        return self.run(lambda: self.sethopperspeed(0))
+    
+    def move(self):
+        return self.run(lambda: self.sethopperspeed(10))
