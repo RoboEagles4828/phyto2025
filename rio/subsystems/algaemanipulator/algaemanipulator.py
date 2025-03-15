3ac3d0b2949d842cc100b8c6825d9dc203c8b614
GoodWaffles10 <henryyqliu@gmail.com>
2025-03-15 18:25:08 +0000

    with commands

--- rio/subsystems/algaemanipulator/algaemanipulator.py
+++ rio/subsystems/algaemanipulator/algaemanipulator.py
@@ -1,6 +1,5 @@
 
-from wpilib import Subsystem
-from commands2 import Command
+from commands2 import Command, ConditionalCommand, Subsystem
 from phoenix5 import TalonSRX, TalonSRXControlMode
 from elevator.elevator import Elevator
 from wpimath.filter import Debouncer
@@ -12,7 +11,7 @@ class AlgaeManipulator(Subsystem):
         self.wheelMotor = TalonSRX(AlgaeConstants.wheelMotorID)
         self.pivotMotor = TalonSRX(AlgaeConstants.pivotMotorID)
 
-        self.stallDebouncer = Debouncer(0.5, Debouncer.DebounceType.kBoth)
+        self.stallDebouncer = Debouncer(0.3, Debouncer.DebounceType.kBoth)
         self.wheelMotor.configSupplyCurrentLimit(AlgaeConstants.supply_config)
         self.pivotMotor.configSupplyCurrentLimit(AlgaeConstants.supply_config)
     
@@ -20,20 +19,29 @@ class AlgaeManipulator(Subsystem):
         """sets motor speed"""
         return motor.set(TalonSRXControlMode.PercentOutput, percentSpeed)
     
-    def stop(self : TalonSRX):
-        self.set(TalonSRXControlMode.PercentOutput, 0.0)
+    def stop(self, motor : TalonSRX) -> Command:   
+        """STOP"""
+        return self.runOnce(lambda: self.setSpeed(motor, 0.0))
 
-    def intake(self, level):
-        self.setSpeed(self.pivotMotor, 0.5)
-        self.setSpeed(self.wheelMotor, 1.0)
+    def intake(self) -> Command:
+        """spin wheel motor"""
+        return self.run(lambda: self.setSpeed(self.wheelMotor, 1.0))
     
-    def outtake(self):
-        self.setSpeed(self.wheelMotor, -1.0)
+    def pivotPosition(self, pos : bool) -> ConditionalCommand: # down = false, up = true
+        return ConditionalCommand(
+            self.run(lambda: self.setSpeed(self.pivotMotor(0.5))), # ontrue
+            self.run(lambda: self.setSpeed(self.pivotMotor(-0.5))), # onfalse
+            pos #bool
+            )
+    
+    def outtake(self) -> Command:
+        return self.run(lambda: self.setSpeed(self.wheelMotor, -1.0))
 
     def pivotStall(self):
         return self.stallDebouncer.calculate(self.pivotMotor.getStatorCurrent() < 10)
 
     def periodic(self):
         if self.pivotStall() == True:
-            self.pivotMotor.stop()
+            self.stop(self.pivotMotor)
+        pass
         
