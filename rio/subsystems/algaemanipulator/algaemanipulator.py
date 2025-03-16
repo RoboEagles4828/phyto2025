19cec86f7016b067239e48de90ebfade6a605071
GoodWaffles10 <henryyqliu@gmail.com>
2025-03-16 22:07:18 +0000

    fixed stop() bug

--- rio/subsystems/algaemanipulator/algaemanipulator.py
+++ rio/subsystems/algaemanipulator/algaemanipulator.py
@@ -1,5 +1,5 @@
 
-from commands2 import Command, ConditionalCommand, Subsystem
+from commands2 import Command, ConditionalCommand, Subsystem, InstantCommand
 from phoenix5 import TalonSRX, TalonSRXControlMode, LimitSwitchSource, LimitSwitchNormal
 #from elevator.elevator import Elevator
 from wpimath.filter import Debouncer
@@ -20,18 +20,25 @@ class AlgaeManipulator(Subsystem):
         self.pivotMotor.configSupplyCurrentLimit(AlgaeConstants.supply_config)
 
     
-    def setSpeed(self, motor : TalonSRX, percentSpeed):
+    def setSpeed(self, motor  : TalonSRX, percentSpeed):
         """sets motor speed"""
         return motor.set(TalonSRXControlMode.PercentOutput, percentSpeed)
     
-    def stop(self, motor : TalonSRX) -> Command:   
+    def stop(self, motor : TalonSRX = None) -> Command:   
         """STOP"""
-        return self.run(lambda: self.setSpeed(motor, 0.0))
+        if motor is None:
+            return self.run(        lambda: self.setSpeed(self.pivotMotor, 0.0)) \
+                        .alongWith( lambda: self.setSpeed(self.wheelMotor, 0.0))
+        else:
+            return self.run(lambda: self.setSpeed(motor, 0.0))
 
     def intake(self) -> Command:
         """spin wheel motor"""
         # figure out speeds and times
-        return self.run(lambda: self.setSpeed(self.wheelMotor, 0.25)).until(lambda: self.wheelStall()).andThen(self.setSpeed(self.pivotMotor, 0.1)).withTimeout(0.1)
+        return self.run(       lambda: self.setSpeed(self.wheelMotor, 0.25))\
+                 .until(       lambda: self.wheelStall()) \
+                 .andThen(     lambda: self.setSpeed(self.pivotMotor, 0.1)) \
+                 .withTimeout(0.1)
     
     # intake sequence - pivot starts down. spins wheel until wheel stall is detected, at which point pivot moves up to grip ball
     
@@ -45,8 +52,8 @@ class AlgaeManipulator(Subsystem):
 
     def pivotPosition(self, pos : bool) -> ConditionalCommand: # down = false, up = true
         return ConditionalCommand(
-            self.run(lambda: self.setSpeed(self.pivotMotor(1.0))).until(self.pivotMotor.isFwdLimitSwitchClosed()), # ontrue
-            self.run(lambda: self.setSpeed(self.pivotMotor(-1.0))).until(self.pivotMotor.isRevLimitSwitchClosed()), # onfalse
+            self.run(lambda: self.setSpeed(self.pivotMotor, 1.0)).until(self.pivotMotor.isFwdLimitSwitchClosed()), # ontrue
+            self.run(lambda: self.setSpeed(self.pivotMotor, 1.0)).until(self.pivotMotor.isRevLimitSwitchClosed()), # onfalse
             pos #bool
             )
     
